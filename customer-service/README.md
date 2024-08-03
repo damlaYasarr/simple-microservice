e215fda559f28f191b3dd286bc4087fda095243a
damlaYasarr <damlayasar40@gmail.com>
2024-08-03 19:36:28 +0300

    customer-service added
    aws postgresql connection build. and customer services function created
    simply.
    
    Signed-off-by: damlaYasarr <damlayasar40@gmail.com>

--- customer-service/README.md
+++ customer-service/README.md
@@ -1,1 +1,14 @@
  ## initialized with golangs
+
+
+
+ * go mod init customer-service  this is the begining of the microservice project part of golang 
+ go mod is the dependency management of golang 
+ 
+ * service api for customer service 
+
+
+
+ * it contains AWS postgresql db
+ 
+ 
---
+++ customer-service/db/aws.go
@@ -0,0 +1,54 @@
+// Use this code snippet in your app.
+// If you need more information about configurations or implementing the sample code, visit the AWS docs:
+// https://aws.github.io/aws-sdk-go-v2/docs/getting-started/
+package db
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/service/secretsmanager"
+	"github.com/joho/godotenv"
+)
+
+func GetSecretValue() map[string]string {
+
+	if err := godotenv.Load(); err != nil {
+		fmt.Println("Error loading .env file:", err)
+		return nil
+	}
+	secretnameaws := os.Getenv("SECRET_NAME")
+	secretName := secretnameaws
+	region := "us-east-1f"
+
+	config, err := config.LoadDefaultConfig(context.TODO(), config.WithRegion(region))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Create Secrets Manager client
+	svc := secretsmanager.NewFromConfig(config)
+
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+
+	result, err := svc.GetSecretValue(context.TODO(), input)
+	if err != nil {
+		// For a list of exceptions thrown, see
+		// https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+		log.Fatal(err.Error())
+	}
+
+	// Decrypts secret using the associated KMS key.
+	var secretMap map[string]string
+	json.Unmarshal([]byte(*result.SecretString), &secretMap)
+
+	return secretMap
+}
